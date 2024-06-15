1ef1e9522630afdcb2c517559035aac4b9bebec0
Peperehobbit01 <contact.peperehobbits01@gmail.com>
2024-06-15 11:57:26 +0200

    Renamed db

--- Bot-setup-database.sql
+++ Bot-setup-database.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `Discord-bot-database-setup`
+-- Base de données : `Discord-bot-database`
 --
 
 -- --------------------------------------------------------

a62f00cd84331b108e9df40c702537aa640904b0
Peperehobbits01 <contact.peperehobbits01@gmail.com>
2024-06-21 16:28:58 +0200

    Added messageCreate.js and  the autoclaim feature (I still have a bug to fix), and removed claimticket.js

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -1,25 +1,71 @@
 const Discord = require("discord.js")
 const { executeQuery } = require("../Fonctions/databaseConnect")
-const { claimingticketpermissions } = require("../Config/TicketConfig")
+const { claimingticketpermissions, panelrole } = require("../Config/TicketConfig")
 
 module.exports = async (bot, message) => {
 
-    const messageSendChannel = message.channel
-    const messageSendChannelSearch = `SELECT * FROM tickets WHERE channelID = '${messageSendChannel}'`
-    const messageSendCHannelResults = await executeQuery(messageSendChannelSearch)
+    if(message.author.bot || message.channel.type === Discord.ChannelType.DM) return;
 
-    if(messageSendCHannelResults.length == 0) return;
-    if(messageSendCHannelResults.length >= 1) {
+    const messageSendUserPermissions = message.member.permissions
 
-        const messageSendUser = message.user
-        const messageSendUserSearch = `SELECT * FROM tickets WHERE userID = '${messageSendUser}'`
-        const messageSendUserResults = await executeQuery(messageSendUserSearch)
+    if(messageSendUserPermissions.has(claimingticketpermissions)) {
 
-        if(messageSendUserResults.length == 0) return;
-        if(messageSendUserResults.length >= 1) {
-            
-            
-        }
-    }
+        const messageSendChannel = message.channel.id
+        const messageSendChannelSearch = `SELECT * FROM tickets WHERE channelID = '${messageSendChannel}'`
+        const messageSendChannelResults = await executeQuery(messageSendChannelSearch)
     
+        if(messageSendChannelResults.length >= 1) {
+    
+            const messageSendUser = message.author.id
+            const messageSendUserSearch = `SELECT * FROM tickets WHERE channelID = '${messageSendChannel}' AND userID = '${messageSendUser}'`
+            const messageSendUserResults = await executeQuery(messageSendUserSearch)
+
+            if(messageSendUserResults.length == 0) {
+                
+                const TicketClaimedSearch = `SELECT * FROM tickets WHERE channelID = '${messageSendChannel}' AND Claimed = '1'`
+                const TicketClaimedResults = await executeQuery(TicketClaimedSearch)
+
+                if(TicketClaimedResults == 0) {
+                    
+                    await message.guild.channel.edit({
+
+                        permissionOverwrites: [
+                            {
+                                id: message.guild.roles.cache.get(panelrole).id,
+                                deny: [
+                                    Discord.PermissionFlagsBits.ViewChannel,
+                                    Discord.PermissionFlagsBits.SendMessages,
+                                    Discord.PermissionFlagsBits.ReadMessageHistory,
+                                    Discord.PermissionFlagsBits.EmbedLinks,
+                                    Discord.PermissionFlagsBits.AttachFiles,
+                                    Discord.PermissionFlagsBits.UseExternalEmojis,
+                                    Discord.PermissionFlagsBits.AddReactions,
+                                    Discord.PermissionFlagsBits.ManageMessages,
+                                    Discord.PermissionFlagsBits.ManageGuildExpressions,
+                                ]
+                            },
+                            {
+                                id: message.author.id,
+                                allow: [
+                                    Discord.PermissionFlagsBits.ViewChannel,
+                                    Discord.PermissionFlagsBits.SendMessages,
+                                    Discord.PermissionFlagsBits.ReadMessageHistory,
+                                    Discord.PermissionFlagsBits.EmbedLinks,
+                                    Discord.PermissionFlagsBits.AttachFiles,
+                                    Discord.PermissionFlagsBits.UseExternalEmojis,
+                                    Discord.PermissionFlagsBits.AddReactions,
+                                    Discord.PermissionFlagsBits.ManageMessages,
+                                    Discord.PermissionFlagsBits.ManageGuildExpressions,
+                                ]
+                            }
+                        ],
+                    });
+
+                    const TicketClaimed = `UPDATE tickets SET Claimed = '1' WHERE channelID = '${messageSendChannel}' AND userID = '${messageSendUser}'`
+                    await executeQuery(TicketClaimed)
+                }
+            }
+        }
+    }  
+    return;
 }
